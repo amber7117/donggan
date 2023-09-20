b1619b206effaa92b2f9a16377db4a9d552ff233
guziyang <guziyang@gmail.com>
2023-09-20 21:09:23 +0800

    fix - 代码提交

--- lib/app/colors.dart
+++ lib/app/colors.dart
@@ -7,5 +7,6 @@ class ThemeColor {
   static const Color black34 = Color.fromRGBO(34, 34, 34, 1.0);
 
   static const Color gray153 = Color.fromRGBO(153, 153, 153, 1.0);
+  static const Color gray248 = Color.fromRGBO(248, 248, 248, 1.0);
   
 }
--- lib/modules/me/page/me_page.dart
+++ lib/modules/me/page/me_page.dart
@@ -13,29 +13,56 @@ class MePage extends StatefulWidget {
 }
 
 class _MePageState extends State {
+
+  final List<MeListItemType> _itemList = [
+    MeListItemType.pingbi,
+    MeListItemType.liulan,
+    MeListItemType.huodong,
+    MeListItemType.wenti,
+    MeListItemType.kefu,
+    MeListItemType.women,
+  ];  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: ScreenUtil().statusBarHeight),
-        color: Colors.yellow,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildCardWidget("me/iconMessage", "消息通知", "0条未读"),
-                _buildCardWidget("me/iconStar", "我的收藏", "收藏赛事"),
-              ],
-            )
-          ],
-        ),
+      backgroundColor: ThemeColor.gray248,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.only(top: ScreenUtil().statusBarHeight),
+              color: Colors.yellow,
+              height: 200,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildCardWidget("me/iconMessage", "消息通知", "0条未读"),
+                      _buildCardWidget("me/iconStar", "我的收藏", "收藏赛事"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverFixedExtentList.list(itemExtent: 64.0, children: [
+            _buildListItemWidget(MeListItemType.pingbi),
+            _buildListItemWidget(MeListItemType.liulan),
+            _buildListItemWidget(MeListItemType.huodong),
+            _buildListItemWidget(MeListItemType.wenti),
+            _buildListItemWidget(MeListItemType.kefu),
+            _buildListItemWidget(MeListItemType.women),
+          ])
+        ],
       ),
     );
   }
 
   _buildCardWidget(String imgPath, String title, String desc) {
     return Container(
+      height: 64.0,
       padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20, right: 40),
       decoration: const BoxDecoration(
           color: Colors.white,
@@ -69,4 +96,52 @@ class _MePageState extends State {
       ),
     );
   }
+
+  _buildListItemWidget(MeListItemType type) {
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: 12),
+      padding: const EdgeInsets.only(top: 20, left: 16, bottom: 20, right: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              JhAssetImage(type.imgPath, width: 24.0, height: 24.0),
+              Text(
+                type.title,
+                style: TextStyle(
+                    color: ThemeColor.black34,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          const JhAssetImage("me/iconMeJiantou", width: 16.0, height: 16.0),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+enum MeListItemType  {
+
+  pingbi(idx: 0, imgPath: "me/iconPingbi", title: "我的屏蔽"),
+  liulan(idx: 1, imgPath: "me/iconJilu", title: "浏览记录"),
+  huodong(idx: 2, imgPath: "me/iconGuangchang", title: "活动广场"),
+  wenti(idx: 3, imgPath: "me/iconWenti", title: "常见问题"),
+  kefu(idx: 4, imgPath: "me/iconKefu", title: "在线客服"),
+  women(idx: 5, imgPath: "me/iconWomen", title: "关于我们");
+
+  const MeListItemType({
+    required this.idx,
+    required this.imgPath,
+    required this.title,
+  });
+
+  final int idx;
+  final String imgPath;
+  final String title;
 }
