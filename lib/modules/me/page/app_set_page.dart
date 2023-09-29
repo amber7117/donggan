7e9fbada8a0102d66e4eb679bb0fba0ba7726136
guziyang <guziyang@gmail.com>
2023-09-29 22:37:06 +0800

    fix - 代码提交

--- lib/modules/me/page/app_set_page.dart
+++ lib/modules/me/page/app_set_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:wzty/common/widget/appbar.dart';
 import 'package:wzty/utils/color_utils.dart';
 import 'package:wzty/utils/jh_image_utils.dart';
+import 'package:wzty/utils/toast_utils.dart';
 
 class AppSetPage extends StatefulWidget {
   const AppSetPage({super.key});
@@ -12,40 +13,57 @@ class AppSetPage extends StatefulWidget {
 }
 
 class _AppSetPageState extends State<AppSetPage> {
+
+  final List<SetListItemType> dataArr = [
+    SetListItemType.appUpdate,
+    SetListItemType.cleanCache
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(titleText: "设置", context: context),
-      body: ListView(
-        physics: const NeverScrollableScrollPhysics(),
-        padding: const EdgeInsets.all(0),
-        itemExtent: 64.0,
-        children: [
-          _buildListItemWidget(SetListItemType.appUpdate),
-          _buildListItemWidget(SetListItemType.cleanCache),
-        ],
-      ),
-    );
+        backgroundColor: ColorUtils.gray248,
+        appBar: buildAppBar(titleText: "设置", context: context),
+        body: ListView.separated(
+          itemCount: dataArr.length,
+          padding: const EdgeInsets.only(top: 10),
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(color: ColorUtils.gray240, indent: 12, height: 0.5),
+          itemBuilder: (BuildContext context, int index) {
+            return _buildListItemWidget(dataArr[index]);
+          },
+        ));
   }
 
   _buildListItemWidget(SetListItemType type) {
-    return Container(
-      padding: const EdgeInsets.only(top: 20, left: 16, bottom: 20, right: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-              padding: const EdgeInsets.only(left: 6),
-              child: Text(
-                type.title,
-                style: TextStyle(
-                    color: ColorUtils.black34,
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w500),
-              )),
-          const JhAssetImage("me/iconMeJiantou", width: 16.0, height: 16.0),
-        ],
+    return InkWell(
+      child: Container(
+        color: Colors.white,
+        height: 56,
+        padding: const EdgeInsets.only(left: 12, right: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Text(
+                  type.title,
+                  style: TextStyle(
+                      color: ColorUtils.black34,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500),
+                )),
+            const JhAssetImage("me/iconMeJiantou", width: 16.0, height: 16.0),
+          ],
+        ),
       ),
+      onTap: () {
+        if (type == SetListItemType.appUpdate) {
+          ToastUtils.showInfo("当前已是最新版本");
+        } else if (type == SetListItemType.cleanCache) {
+          
+        }
+      },
     );
   }
 }
