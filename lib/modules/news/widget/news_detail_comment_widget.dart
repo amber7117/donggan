0833c113566f3aa6eb9c9e0bb679bd4787ab9921
guziyang <guziyang@gmail.com>
2023-10-15 00:44:03 +0800

    fix - 代码提交

---
+++ lib/modules/news/widget/news_detail_comment_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailCommentWidget extends StatefulWidget {
+  const NewsDetailCommentWidget({super.key});
+
+  @override
+  State createState() => _NewsDetailCommentWidgetState();
+}
+
+class _NewsDetailCommentWidgetState extends State<NewsDetailCommentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/modules/news/widget/news_detail_header_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailHeaderWidget extends StatefulWidget {
+  const NewsDetailHeaderWidget({super.key});
+
+  @override
+  State createState() => _NewsDetailHeaderWidgetState();
+}
+
+class _NewsDetailHeaderWidgetState extends State<NewsDetailHeaderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
